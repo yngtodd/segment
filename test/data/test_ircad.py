a7602067c294901903aaa70b88cc136358c9c089
yngtodd <young.todd.mk@gmail.com>
2018-09-05 21:47:59 -0400

    Adding experiments info

---
+++ test/data/test_ircad.py
@@ -0,0 +1,18 @@
+"""
+Tests for `segment` module.
+"""
+import pytest
+
+
+class TestSegment(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
