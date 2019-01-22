08abb498db3c80a059eeba6edc7e05b728dc33ab
yngtodd <young.todd.mk@gmail.com>
2019-01-22 12:26:13 -0500

    reference before assignment

--- segment/ml/functional.py
+++ segment/ml/functional.py
@@ -71,7 +71,7 @@ def dice_coefficient(input, target):
 
 def dice_score(prediction, groundtruth):
     prediction = torch.sigmoid(prediction)
-    pred = pred.cpu().numpy()
+    pred = prediction.cpu().numpy()
     pred = threshold_predictions(pred)
     groundtruth = groundtruth.cpu().numpy()
     pflat = pred.flatten()
