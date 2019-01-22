43f85e88b3c13ad5dcf9ff56326304dec62509bd
yngtodd <young.todd.mk@gmail.com>
2019-01-22 13:00:16 -0500

    new dice loss

--- segment/ml/functional.py
+++ segment/ml/functional.py
@@ -154,11 +154,43 @@ def iou_pytorch(output: torch.Tensor, target: torch.Tensor):
     return thresholded
 
 
-def threshold_predictions(predictions, thr=0.999):
-    """Threshold predictions for dice"""
-    thresholded_preds = predictions[:]
-    low_values_indices = thresholded_preds < thr
-    thresholded_preds[low_values_indices] = 0
-    low_values_indices = thresholded_preds >= thr
-    thresholded_preds[low_values_indices] = 1
-    return thresholded_preds
+def dice_loss(true, logits, eps=1e-7):
+    """Computes the Sørensen–Dice loss.
+
+    Note that PyTorch optimizers minimize a loss. In this
+    case, we would like to maximize the dice loss so we
+    return the negated dice loss.
+
+    Parameters
+    ----------
+        true: a tensor of shape [B, 1, H, W].
+
+        logits: a tensor of shape [B, C, H, W]. Corresponds to
+            the raw output or logits of the model.
+
+        eps: added to the denominator for numerical stability.
+
+    Returns
+    -------
+        dice_loss: the Sørensen–Dice loss.
+    """
+    num_classes = logits.shape[1]
+    if num_classes == 1:
+        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        true_1_hot_f = true_1_hot[:, 0:1, :, :]
+        true_1_hot_s = true_1_hot[:, 1:2, :, :]
+        true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
+        pos_prob = torch.sigmoid(logits)
+        neg_prob = 1 - pos_prob
+        probas = torch.cat([pos_prob, neg_prob], dim=1)
+    else:
+        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        probas = F.softmax(probas, dim=1)
+
+    dims = (0,) + tuple(range(2, true.ndimension()))
+    intersection = torch.sum(probas * true_1_hot, dims)
+    cardinality = torch.sum(probas + true_1_hot, dims)
+    dice = (2. * intersection / (cardinality + eps)).mean()
+    return (1 - dice), dice
